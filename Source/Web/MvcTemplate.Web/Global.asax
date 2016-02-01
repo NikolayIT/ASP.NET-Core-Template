a987fcb10f8ed2b9b9f5220852ff6bda8656232d
Nikolay Kostov <admin@nikolay.it>
2016-02-01 20:05:31 +0200

    Created MvcTemplate.Web project (default ASP.NET MVC template project)

---
+++ Source/Web/MvcTemplate.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcTemplate.Web.MvcApplication" Language="C#" %>
---
+++ Source/Web/MvcTemplate.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcTemplate.Web.Startup))]
+namespace MvcTemplate.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
