c6211aca68e5655834cab46f4989718c13c2d148
Nikolay Kostov <admin@nikolay.it>
2016-02-01 20:54:06 +0200

    Installed Glimpse and removed useless Project_Readme.html

--- Source/Web/MvcTemplate.Web/Controllers/HomeController.cs
+++ Source/Web/MvcTemplate.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcTemplate.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,8 @@ namespace MvcTemplate.Web.Controllers
     {
         public ActionResult Index()
         {
+            var db = new ApplicationDbContext();
+            var usersCount = db.Users.Count();
             return View();
         }
 
