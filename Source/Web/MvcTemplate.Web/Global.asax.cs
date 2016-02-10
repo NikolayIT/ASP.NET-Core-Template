4dacc286fb2f5b05a755fa9ef1a80856d8cfe3ce
Nikolay Kostov <admin@nikolay.it>
2016-02-10 16:11:03 +0200

    Only razor view engine left

--- Source/Web/MvcTemplate.Web/Global.asax.cs
+++ Source/Web/MvcTemplate.Web/Global.asax.cs
@@ -16,6 +16,9 @@
     {
         protected void Application_Start()
         {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
             AutofacConfig.RegisterAutofac();
             AreaRegistration.RegisterAllAreas();
