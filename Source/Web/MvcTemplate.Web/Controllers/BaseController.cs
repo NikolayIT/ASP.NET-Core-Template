2bd0a70222df1c8798901d7e7cec9d640bd688f8
danisio <dany_s@abv.bg>
2016-02-11 13:45:57 +0200

    Extracted property Mapper in BaseController

--- Source/Web/MvcTemplate.Web/Controllers/BaseController.cs
+++ Source/Web/MvcTemplate.Web/Controllers/BaseController.cs
@@ -1,11 +1,20 @@
 ﻿namespace MvcTemplate.Web.Controllers
 {
     using System.Web.Mvc;
-
+    using AutoMapper;
+    using Infrastructure.Mapping;
     using MvcTemplate.Services.Web;
 
     public abstract class BaseController : Controller
     {
         public ICacheService Cache { get; set; }
+
+        protected IMapper Mapper
+        {
+            get
+            {
+                return AutoMapperConfig.Configuration.CreateMapper();
+            }
+        }
     }
 }
