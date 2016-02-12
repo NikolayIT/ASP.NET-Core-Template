e367eb9a7111afffecf462a7e9bea4e5de8ffb38
Nikolay Kostov <admin@nikolay.it>
2016-02-13 00:59:40 +0200

    Admin role and area added

--- Source/Data/MvcTemplate.Data/Migrations/Configuration.cs
+++ Source/Data/MvcTemplate.Data/Migrations/Configuration.cs
@@ -1,6 +1,13 @@
 ﻿namespace MvcTemplate.Data.Migrations
 {
     using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    using MvcTemplate.Common;
+    using MvcTemplate.Data.Models;
 
     public sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
@@ -12,6 +19,26 @@
 
         protected override void Seed(ApplicationDbContext context)
         {
+            const string AdministratorUserName = "admin@admin.com";
+            const string AdministratorPassword = AdministratorUserName;
+
+            if (!context.Roles.Any())
+            {
+                // Create admin role
+                var roleStore = new RoleStore<IdentityRole>(context);
+                var roleManager = new RoleManager<IdentityRole>(roleStore);
+                var role = new IdentityRole { Name = GlobalConstants.AdministratorRoleName };
+                roleManager.Create(role);
+
+                // Create admin user
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var user = new ApplicationUser { UserName = AdministratorUserName, Email = AdministratorUserName };
+                userManager.Create(user, AdministratorPassword);
+
+                // Assign user to admin role
+                userManager.AddToRole(user.Id, GlobalConstants.AdministratorRoleName);
+            }
         }
     }
 }
---
+++ Source/Web/MvcTemplate.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,12 @@
+﻿namespace MvcTemplate.Web.Areas.Administration.Controllers
+{
+    using System.Web.Mvc;
+
+    using MvcTemplate.Common;
+    using MvcTemplate.Web.Controllers;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    public class AdministrationController : BaseController
+    {
+    }
+}
