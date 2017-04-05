78cfa2fc8b42cb3806ce956ddcd4b104c2b29600
Nikolay Kostov <admin@nikolay.it>
2017-04-05 15:42:00 +0300

    Added default ASP.NET Core 1.1 project in Visual Studio 2017 with Individual User Accounts

---
+++ ASP.NET Core/Web/AspNetCoreTemplate.Web/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTemplate.Web.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
