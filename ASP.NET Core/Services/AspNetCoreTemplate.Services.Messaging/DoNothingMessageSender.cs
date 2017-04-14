fc5cb313a227765c76b6c4a0b19fe4746e5e0f78
Nikolay Kostov <admin@nikolay.it>
2017-04-14 15:34:05 +0300

    Implemented SendGrid functionality for sending emails

--- ASP.NET Core/Services/AspNetCoreTemplate.Services.Messaging/DoNothingMessageSender.cs
+++ ASP.NET Core/Services/AspNetCoreTemplate.Services.Messaging/DoNothingMessageSender.cs
@@ -5,7 +5,7 @@
     // This class is used by the application to send Email and SMS
     // when you turn on two-factor authentication in ASP.NET Identity.
     // For more details see this link https://go.microsoft.com/fwlink/?LinkID=532713
-    public class AuthMessageSender : IEmailSender, ISmsSender
+    public class DoNothingMessageSender : IEmailSender, ISmsSender
     {
         public Task SendEmailAsync(string email, string subject, string message)
         {
--- ASP.NET Core/Web/AspNetCoreTemplate.Web/Startup.cs
+++ ASP.NET Core/Web/AspNetCoreTemplate.Web/Startup.cs
@@ -55,8 +55,8 @@
             services.AddMvc();
 
             // Add application services.
-            services.AddTransient<IEmailSender, AuthMessageSender>();
-            services.AddTransient<ISmsSender, AuthMessageSender>();
+            services.AddTransient<IEmailSender, DoNothingMessageSender>();
+            services.AddTransient<ISmsSender, DoNothingMessageSender>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
