90f8332673ed1e088ea0caf86e0b883e8cf51272
Nikolay Kostov <admin@nikolay.it>
2017-04-20 20:09:51 +0300

    Added infrastructure files for mapping functionality using AutoMapper 6.0.2

---
+++ ASP.NET Core/Web/AspNetCoreTemplate.Web.Infrastructure/Mapping/TypesMap.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetCoreTemplate.Web.Infrastructure.Mapping
+{
+    using System;
+
+    internal class TypesMap
+    {
+        public Type Source { get; set; }
+
+        public Type Destination { get; set; }
+    }
+}
