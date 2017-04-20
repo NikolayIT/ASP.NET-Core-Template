feb6c5929bab6b3cc870cef9a0e55e9c4fb946f2
Nikolay Kostov <admin@nikolay.it>
2017-04-20 20:40:33 +0300

    Created a simple list of settings to demonstrate how repositories and mapping works in the template

---
+++ ASP.NET Core/Web/AspNetCoreTemplate.Web/Controllers/SettingsController.cs
@@ -0,0 +1,28 @@
+﻿namespace AspNetCoreTemplate.Web.Controllers
+{
+    using System.Linq;
+
+    using AspNetCoreTemplate.Data.Common.Repositories;
+    using AspNetCoreTemplate.Data.Models;
+    using AspNetCoreTemplate.Web.Infrastructure.Mapping;
+    using AspNetCoreTemplate.Web.Models.Settings;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class SettingsController : Controller
+    {
+        private readonly IDbRepository<Setting> repository;
+
+        public SettingsController(IDbRepository<Setting> repository)
+        {
+            this.repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            var settings = this.repository.All().To<SettingViewModel>().ToList();
+            var model = new SettingsListViewModel { Settings = settings };
+            return this.View(model);
+        }
+    }
+}
---
+++ ASP.NET Core/Web/AspNetCoreTemplate.Web/Models/Settings/SettingViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AspNetCoreTemplate.Web.Models.Settings
+{
+    using AspNetCoreTemplate.Data.Models;
+    using AspNetCoreTemplate.Web.Infrastructure.Mapping;
+
+    public class SettingViewModel : IMapFrom<Setting>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
