9f5d37f189d2f29cfcfd6a45001dc8aaef4f6ad0
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2017-04-28 10:17:57 +0300

    Improvements in web project startup

--- ASP.NET Core/Web/AspNetCoreTemplate.Web/Startup.cs
+++ ASP.NET Core/Web/AspNetCoreTemplate.Web/Startup.cs
@@ -21,53 +21,56 @@
 
     public class Startup
     {
+        private readonly IConfigurationRoot configuration;
+
         public Startup(IHostingEnvironment env)
         {
-            var builder = new ConfigurationBuilder()
+            var configurationBuilder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
             if (env.IsDevelopment())
             {
-                builder.AddUserSecrets<Startup>();
+                configurationBuilder.AddUserSecrets<Startup>();
             }
 
-            builder.AddEnvironmentVariables();
-            this.Configuration = builder.Build();
-        }
+            configurationBuilder.AddEnvironmentVariables();
 
-        public IConfigurationRoot Configuration { get; }
+            this.configuration = configurationBuilder.Build();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
+            // Framework services
             services.AddDbContext<ApplicationDbContext>(
-                options => options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
+                options => options.UseSqlServer(this.configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, ApplicationRole>(
-                    options =>
-                    {
-                        options.Password.RequireDigit = false;
-                        options.Password.RequireLowercase = false;
-                        options.Password.RequireUppercase = false;
-                        options.Password.RequireNonAlphanumeric = false;
-                        options.Password.RequiredLength = 6;
-                    })
+                options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequiredLength = 6;
+                })
                 .AddUserStore<ApplicationUserStore>()
                 .AddRoleStore<ApplicationRoleStore>()
                 .AddDefaultTokenProviders();
 
             services.AddMvc();
 
+            services.AddSingleton(this.configuration);
+
             // Data
             services.AddScoped(typeof(IDeletableEntityRepository<>), typeof(EfDeletableEntityRepository<>));
             services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
 
-            // Add application services.
-            services.AddTransient<IEmailSender, DoNothingMessageSender>();
-            services.AddTransient<ISmsSender, DoNothingMessageSender>();
+            // Application services
+            services.AddTransient<IEmailSender, NullMessageSender>();
+            services.AddTransient<ISmsSender, NullMessageSender>();
 
             // Identity stores
             services.AddTransient<IUserStore<ApplicationUser>, ApplicationUserStore>();
@@ -78,18 +81,16 @@
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             AutoMapperConfig.RegisterMappings(
-                typeof(LoginViewModel).GetTypeInfo().Assembly,
                 typeof(LoginViewModel).GetTypeInfo().Assembly);
 
             // Seed data on application startup
             using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
             {
-                var db = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                var seeder = new ApplicationDbContextSeeder();
-                seeder.Seed(db, app.ApplicationServices);
+                var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                ApplicationDbContextSeeder.Seed(dbContext, app.ApplicationServices);
             }
 
-            loggerFactory.AddConsole(this.Configuration.GetSection("Logging"));
+            loggerFactory.AddConsole(this.configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
             if (env.IsDevelopment())
@@ -107,9 +108,9 @@
             app.UseIdentity();
 
             app.UseMvc(routes =>
-                {
-                    routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
-                });
+            {
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
