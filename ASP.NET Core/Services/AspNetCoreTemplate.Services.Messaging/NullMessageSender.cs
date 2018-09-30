3564ce3ae6989b478e614cb5fd5462d1fd2e59b2
Georgi Manov <georgimanov@gmail.com>
2018-09-30 16:32:16 +0300

    Implemented IEmailSender

--- ASP.NET Core/Services/AspNetCoreTemplate.Services.Messaging/NullMessageSender.cs
+++ ASP.NET Core/Services/AspNetCoreTemplate.Services.Messaging/NullMessageSender.cs
@@ -2,6 +2,8 @@
 {
     using System.Threading.Tasks;
 
+    using Microsoft.AspNetCore.Identity.UI.Services;
+
     // This class is used by the application to send Email and SMS
     // when you turn on two-factor authentication in ASP.NET Identity.
     // For more details see this link https://go.microsoft.com/fwlink/?LinkID=532713
