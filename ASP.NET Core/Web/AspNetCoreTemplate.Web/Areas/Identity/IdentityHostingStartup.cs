b6a0b6bf24a8e511d09ea92ff367e910d8118fc4
Georgi Manov <georgimanov@gmail.com>
2018-09-30 15:26:23 +0300

    Scaffolded Identity

---
+++ ASP.NET Core/Web/AspNetCoreTemplate.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AspNetCoreTemplate.Data;
+using AspNetCoreTemplate.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AspNetCoreTemplate.Web.Areas.Identity.IdentityHostingStartup))]
+namespace AspNetCoreTemplate.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
