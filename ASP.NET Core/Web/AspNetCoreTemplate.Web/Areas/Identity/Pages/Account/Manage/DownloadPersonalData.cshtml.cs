599b3fd3fcce663073d6b74e9713f275ccdcc391
Vladislav Karamfilov <vladislav.karamfilov@gmail.com>
2018-10-04 23:45:42 +0300

    Update DownloadPersonalData.cshtml.cs

--- ASP.NET Core/Web/AspNetCoreTemplate.Web/Areas/Identity/Pages/Account/Manage/DownloadPersonalData.cshtml.cs
+++ ASP.NET Core/Web/AspNetCoreTemplate.Web/Areas/Identity/Pages/Account/Manage/DownloadPersonalData.cshtml.cs
@@ -7,6 +7,7 @@
     using System.Threading.Tasks;
 
     using AspNetCoreTemplate.Data.Models;
+
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
