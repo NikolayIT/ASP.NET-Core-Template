802d3db6012d68d315c6b896644b269d3658fdd4
Nikolay Kostov <admin@nikolay.it>
2018-11-02 15:00:00 +0200

    Updated bootstrap to version 4.1.3

--- ASP.NET MVC 5/Web/MvcTemplate.Web/App_Start/BundleConfig.cs
+++ ASP.NET MVC 5/Web/MvcTemplate.Web/App_Start/BundleConfig.cs
@@ -15,7 +15,7 @@
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js").Include("~/Scripts/popper.js"));
         }
 
         private static void RegisterStyles(BundleCollection bundles)
