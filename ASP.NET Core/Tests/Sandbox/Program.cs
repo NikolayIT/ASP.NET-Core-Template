7298040135f46024f61391ca37c3605edbbf125e
Nikolay Kostov <admin@nikolay.it>
2018-12-03 19:27:00 +0200

    [ASP.NET Core] Setup Sandbox project for experimenting

---
+++ ASP.NET Core/Tests/Sandbox/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace Sandbox
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+
+    using AspNetCoreTemplate.Data;
+    using AspNetCoreTemplate.Data.Common;
+    using AspNetCoreTemplate.Data.Common.Repositories;
+    using AspNetCoreTemplate.Data.Models;
+    using AspNetCoreTemplate.Data.Repositories;
+    using AspNetCoreTemplate.Data.Seeding;
+    using AspNetCoreTemplate.Services.Data;
+    using AspNetCoreTemplate.Services.Messaging;
+
+    using CommandLine;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.UI.Services;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine($"{typeof(Program).Namespace} ({string.Join(" ", args)}) starts working...");
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider(true);
+
+            // Seed data on application startup
+            using (var serviceScope = serviceProvider.CreateScope())
+            {
+                var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                dbContext.Database.Migrate();
+                ApplicationDbContextSeeder.Seed(dbContext, serviceScope.ServiceProvider);
+            }
+
+            using (var serviceScope = serviceProvider.CreateScope())
+            {
+                serviceProvider = serviceScope.ServiceProvider;
+
+                return Parser.Default.ParseArguments<SandboxOptions>(args).MapResult(
+                    (SandboxOptions opts) => SandboxCode(opts, serviceProvider),
+                    _ => 255);
+            }
+        }
+
+        private static int SandboxCode(SandboxOptions options, IServiceProvider serviceProvider)
+        {
+            var sw = Stopwatch.StartNew();
+            var settingsService = serviceProvider.GetService<ISettingsService>();
+            Console.WriteLine($"Count of settings: {settingsService.GetCount()}");
+            Console.WriteLine(sw.Elapsed);
+            return 0;
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            services.AddSingleton<IConfiguration>(configuration);
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+                    .UseLoggerFactory(new LoggerFactory()));
+
+            services
+                .AddIdentity<ApplicationUser, ApplicationRole>(options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequiredLength = 6;
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddUserStore<ApplicationUserStore>()
+                .AddRoleStore<ApplicationRoleStore>()
+                .AddDefaultTokenProviders();
+
+            services.AddScoped(typeof(IDeletableEntityRepository<>), typeof(EfDeletableEntityRepository<>));
+            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+            services.AddScoped<IDbQueryRunner, DbQueryRunner>();
+
+            // Application services
+            services.AddTransient<IEmailSender, NullMessageSender>();
+            services.AddTransient<ISmsSender, NullMessageSender>();
+            services.AddTransient<ISettingsService, SettingsService>();
+        }
+    }
+}
---
+++ ASP.NET Core/Tests/Sandbox/SandboxOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Sandbox
+{
+    using CommandLine;
+
+    [Verb("sandbox", HelpText = "Run sandbox code.")]
+    public class SandboxOptions
+    {
+    }
+}
