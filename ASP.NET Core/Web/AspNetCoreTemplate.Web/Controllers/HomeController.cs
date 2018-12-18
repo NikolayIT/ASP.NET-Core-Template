508c5fb21251456505bf2079eae7eccd95569b31
Nikolay Kostov <admin@nikolay.it>
2018-12-18 21:34:35 +0200

    Main views and styles updated for Bootstrap 4

--- ASP.NET Core/Web/AspNetCoreTemplate.Web/Controllers/HomeController.cs
+++ ASP.NET Core/Web/AspNetCoreTemplate.Web/Controllers/HomeController.cs
@@ -9,6 +9,11 @@
             return this.View();
         }
 
+        public IActionResult Privacy()
+        {
+            return this.View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() => this.View();
     }
--- ASP.NET Core/Web/AspNetCoreTemplate.Web/Startup.cs
+++ ASP.NET Core/Web/AspNetCoreTemplate.Web/Startup.cs
@@ -11,7 +11,7 @@
     using AspNetCoreTemplate.Services.Data;
     using AspNetCoreTemplate.Services.Mapping;
     using AspNetCoreTemplate.Services.Messaging;
-    using AspNetCoreTemplate.Web.ViewModels.Settings;
+    using AspNetCoreTemplate.Web.ViewModels;
 
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
@@ -104,7 +104,7 @@
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            AutoMapperConfig.RegisterMappings(typeof(SettingViewModel).GetTypeInfo().Assembly);
+            AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
 
             // Seed data on application startup
             using (var serviceScope = app.ApplicationServices.CreateScope())
