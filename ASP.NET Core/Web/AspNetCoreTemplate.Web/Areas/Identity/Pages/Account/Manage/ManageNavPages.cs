eeac59bf7b1acf5d308e865ed916bb072bc9d422
Nikolay Kostov <admin@nikolay.it>
2018-12-18 20:36:47 +0200

    All views are updated now

--- ASP.NET Core/Web/AspNetCoreTemplate.Web/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ ASP.NET Core/Web/AspNetCoreTemplate.Web/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -10,10 +10,6 @@
 
         public static string ChangePassword => "ChangePassword";
 
-        public static string DownloadPersonalData => "DownloadPersonalData";
-
-        public static string DeletePersonalData => "DeletePersonalData";
-
         public static string ExternalLogins => "ExternalLogins";
 
         public static string PersonalData => "PersonalData";
@@ -24,17 +20,13 @@
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
 
-        public static string DownloadPersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, DownloadPersonalData);
-
-        public static string DeletePersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, DeletePersonalData);
-
         public static string ExternalLoginsNavClass(ViewContext viewContext) => PageNavClass(viewContext, ExternalLogins);
 
         public static string PersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, PersonalData);
 
         public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
 
-        public static string PageNavClass(ViewContext viewContext, string page)
+        private static string PageNavClass(ViewContext viewContext, string page)
         {
             var activePage = viewContext.ViewData["ActivePage"] as string
                 ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
