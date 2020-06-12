c73d000d7ba27c6b4b1c2ea479cb2fac0fef093f
IliyanAng <focusworking@gmail.com>
2020-06-12 14:53:37 +0300

    Slight optimizations
    Adding the ''using'' keyword to handle the IDisposable ApplicationDbContext & EfDeletableEntityRepository
    
    Using the .AllAsNoTracking() repository method for when getting the settings count (as it's a read only operation)

--- src/Tests/AspNetCoreTemplate.Services.Data.Tests/SettingsServiceTests.cs
+++ src/Tests/AspNetCoreTemplate.Services.Data.Tests/SettingsServiceTests.cs
@@ -43,7 +43,7 @@
             dbContext.Settings.Add(new Setting());
             await dbContext.SaveChangesAsync();
 
-            var repository = new EfDeletableEntityRepository<Setting>(dbContext);
+            using var repository = new EfDeletableEntityRepository<Setting>(dbContext);
             var service = new SettingsService(repository);
             Assert.Equal(3, service.GetCount());
         }
