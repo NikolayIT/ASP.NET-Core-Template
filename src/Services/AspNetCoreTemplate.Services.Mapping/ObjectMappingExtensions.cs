e9aee0bb2b25b7a0f71c62145db5b00a20cfacc0
Georgi Petrov <78408524+p3trovbg@users.noreply.github.com>
2023-04-27 17:49:04 +0300

    Create ObjectMappingExtensions.cs

---
+++ src/Services/AspNetCoreTemplate.Services.Mapping/ObjectMappingExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace AspNetCoreTemplate.Services.Mapping
+{
+    using System;
+    using System.Linq.Expressions;
+
+    public static class ObjectMappingExtensions
+    {
+        public static TDestination To<TDestination>(
+           this object source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return AutoMapperConfig.MapperInstance.Map<TDestination>(source);
+        }
+    }
+}
